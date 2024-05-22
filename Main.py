b608eac6aba561bedd87162aaa8be88777ccb88e
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-22 15:08:40 -0300

    feat: malandramente lirics

--- Main.py
+++ Main.py
@@ -21,21 +21,37 @@ led_7 = LED(12)
 
 BEAT = 0.3  
 
-##liten_mus = [ ['d5', BEAT / 2], ['d#5', BEAT / 2], ['f5', BEAT], ['d6', BEAT], ['a#5', BEAT], ['d5', BEAT],
-##              ['f5', BEAT], ['d#5', BEAT], ['d#5', BEAT], ['c5', BEAT / 2],['d5', BEAT / 2], ['d#5', BEAT],
-##              ['c6', BEAT], ['a5', BEAT], ['d5', BEAT], ['g5', BEAT], ['f5', BEAT], ['f5', BEAT], ['d5', BEAT / 2],
-##              ['d#5', BEAT / 2], ['f5', BEAT], ['g5', BEAT], ['a5', BEAT], ['a#5', BEAT], ['a5', BEAT], ['g5', BEAT],
-##              ['g5', BEAT], ['', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT / 2], ['d6', BEAT / 2], ['c6', BEAT / 2],
-##              ['a#5', BEAT / 2], ['a5', BEAT / 2], ['g5', BEAT / 2], ['a5', BEAT / 2], ['a#5', BEAT / 2], ['c6', BEAT],
-##              ['f5', BEAT], ['f5', BEAT], ['f5', BEAT / 2], ['d#5', BEAT / 2], ['d5', BEAT], ['f5', BEAT], ['d6', BEAT],
-##              ['d6', BEAT / 2], ['c6', BEAT / 2], ['b5', BEAT], ['g5', BEAT], ['g5', BEAT], ['c6', BEAT / 2],
-##              ['a#5', BEAT / 2], ['a5', BEAT], ['f5', BEAT], ['d6', BEAT], ['a5', BEAT], ['a#5', BEAT * 1.5] ]
+liten_mus = [
+    ['e4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['e4', BEAT],  # malandramento
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # a menina inocente
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # se envolveu com a gente
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['d4', BEAT], ['d4', BEAT],  # só pra poder curtir
+
+    ['e4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['e4', BEAT],  # malandramento
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # fez cara de carente
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # envolvida com a tropa
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['d4', BEAT], ['d4', BEAT],  # começou a seduzir
+
+    ['e4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['e4', BEAT],  # malandramento
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # meteu o pé pra casa
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['g4', BEAT], ['e4', BEAT],  # diz que a mãe tá ligando
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['d4', BEAT], ['d4', BEAT],  # nós se vê por aí
+
+    ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['e4', BEAT],  # ai safada!
+
+    ['e4', BEAT], ['e4', BEAT], ['c4', BEAT], ['c4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['a4', BEAT], ['g4', BEAT], ['a4', BEAT], ['e4', BEAT],  # na hora de ganhar madeirada
+    ['e4', BEAT], ['e4', BEAT], ['c4', BEAT], ['c4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['a4', BEAT], ['g4', BEAT], ['a4', BEAT], ['e4', BEAT],  # a menina meteu o pé pra casa
+    ['e4', BEAT], ['c4', BEAT], ['c4', BEAT], ['c4', BEAT], ['b4', BEAT], ['a4', BEAT], ['a4', BEAT], ['g4', BEAT], ['a4', BEAT],  # e mandou um recadinho pra mim
+
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['d4', BEAT], ['d4', BEAT],  # nós se vê por aí
+    ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['e4', BEAT], ['d4', BEAT], ['d4', BEAT]   # nós se vê por aí
+]
 
 
-liten_mus = [['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
-             ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
-             ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
-             ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT]]
+# liten_mus = [['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
+#              ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
+#              ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT],
+#              ['c4', BEAT], ['d4', BEAT], ['c4', BEAT], ['c4', BEAT]]
 
 def c4():
     led_1.on()
